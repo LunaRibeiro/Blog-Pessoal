3c4b637f06573d756e35e13ab7a39aabef91bbf5
Luna Ribeiro <lunasantos@alunos.utfpr.edu.br>
2022-02-18 11:46:29 -0300

    Versão 2.4

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,14 +61,17 @@ public class PostagemController {
 	}
 	
 	
-	//---------------------------------------- deletar ---------------------------------------------------------
+	//---------------------------------------- editar ---------------------------------------------------------
 	@PutMapping
 	public ResponseEntity<Postagem> putPostagem (@Valid @RequestBody Postagem postagem){
-		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));	//salva no banco
+		return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));	//salva no banco
 	}
 	
-	
-	
+	//---------------------------------------- deletar ---------------------------------------------------------
+	@DeleteMapping("/{id}")
+	public void deletePostagem(@PathVariable Long id) {
+		postagemRepository.deleteById(id);
+	}
 	
 	
 	
